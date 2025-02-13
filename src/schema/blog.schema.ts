5ca2010520dcedc0d41eeba888c9c1fac09617a3
LowerX Dev 🔥 <167161254+lowerx-dev@users.noreply.github.com>
2025-02-13 19:16:59 +0700

    Fix "Content" Min ✅

--- src/schema/blog.schema.ts
+++ src/schema/blog.schema.ts
@@ -5,7 +5,7 @@ export const createBlogSchema = z.object({
         .min(5, 'ชื่อเรื่องต้องมีอย่างน้อย 10 ตัวอักษร')
         .max(256, 'ชื่อเรื่องตัวอักษรมากสุดได้เพียง 256 ตัวอักษร'),
     content: z.string()
-        .min(15, 'เนื้อหาต้องมีอักขระอย่างน้อย 150 ตัว')
+        .min(150, 'เนื้อหาต้องมีอักขระอย่างน้อย 150 ตัว')
 });
 
 export type CreateBlogSchema = z.infer<typeof createBlogSchema>;
